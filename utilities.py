71463cab9c8266d61188f3fe3ad786dc78e3475c
Rommel <giostrasser@gmail.com>
2023-11-17 10:44:59 -0300

    code clean up

--- utilities.py
+++ utilities.py
@@ -3,10 +3,3 @@ import os
 def clear_console():
     os.system('cls')
     return
-
-def transaction(value, bills): #Returns true or false if transaction is possible
-    return
-
-
-def add_to_catalogue():
-    return
